9a5f2636f369a32aae4345c18ff2810fbc5dc201
Fatih Kabay <mfthkabay@gmail.com>
2023-01-31 22:20:18 +0300

    create community logo

--- community-fe/src/pages/Site/index.tsx
+++ community-fe/src/pages/Site/index.tsx
@@ -1,18 +1,38 @@
-import { Input, Form, Button } from "antd";
-import "./site.css";
+import React from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
-function Site() {
+const { Header, Content, Footer, Sider } = Layout;
+
+
+const Site: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   return (
-    <Form 
-    className="site-page">
-      <Form.Item>
-        <Button  htmlType="submit">Logout</Button>
-        <Input />
-        <Button htmlType="submit">Buying Car</Button>
-        <Button htmlType="submit">Add to Basket</Button>
-      </Form.Item>
-    </Form>
-    )
- }
- export default Site;
+
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} />
+        <Button htmlType='submit'>Logout</Button>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Login</Breadcrumb.Item>
+          <Breadcrumb.Item>Site</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout style={{ padding: '24px 0', background: colorBgContainer }}>
+          
+          <Content style={{ padding: '0 24px', minHeight: 600 }}>Content</Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Community ©2023 Created by Fatih Kabay</Footer>
+    </Layout>
+  );
+};
+
+export default Site;
--- community-fe/src/utils/helpers.ts
+++ community-fe/src/utils/helpers.ts
@@ -2,12 +2,13 @@ import { UserModel } from "../services/User/Models";
 
 export function getUser(): UserModel | undefined {
     const user = localStorage.getItem('user');
-    
+    console.log(user);
     if (user == null) {
         return undefined;
     }
 
     return JSON.parse(user);
+    
 }
   
 export function setUser(user: UserModel): void {
