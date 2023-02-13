9d5b496f032c0fd820f29cad18d681488cfa93bd
Fatih Kabay <mfthkabay@gmail.com>
2023-02-13 21:20:41 +0300

    added delete function

--- community-fe/src/services/Car/CarService.ts
+++ community-fe/src/services/Car/CarService.ts
@@ -6,5 +6,9 @@ class CarService {
        const find = await axios.post('http://localhost:3030/car/car/get', newCar)
        return find.data;
     }
+    async deleteCar(deleteCar: any) {
+        const deleteC = await axios.delete('',deleteCar)
+        return deleteC.data
+    }
 }
 export default new CarService();
