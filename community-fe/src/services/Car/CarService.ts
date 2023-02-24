00bf0a7140014f2291d3b5fb142bb228d918e4b0
Fatih Kabay <mfthkabay@gmail.com>
2023-02-24 21:22:23 +0300

    car functions is changed name

--- community-fe/src/services/Car/CarService.ts
+++ community-fe/src/services/Car/CarService.ts
@@ -3,17 +3,17 @@ import { CarOutputModel, GetCarInputModel } from '../Car/Models';
 
 class CarService {
     async create(newCar: GetCarInputModel): Promise<CarOutputModel> {
-       const createCar = await axios.post('http://localhost:3030/car/car/create', newCar)
+       const createCar = await axios.post('http://localhost:3030/car/create', newCar)
        return createCar.data;
     }
 
     async update(updateCar: GetCarInputModel): Promise<CarOutputModel> {
-        const updateC = await axios.put('http://localhost:3030/car/car/update', updateCar);
+        const updateC = await axios.put('http://localhost:3030/car/update', updateCar);
         return updateC.data;
     }
 
     async deleteCar(deleteCar: any) {
-        const deleteC = await axios.delete('http://localhost:3030/car/car/delete', deleteCar)
+        const deleteC = await axios.delete('http://localhost:3030/car/delete', deleteCar)
         return deleteC.data;
     }
 }
