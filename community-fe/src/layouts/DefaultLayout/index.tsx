a5272d83eb029c391ad03c39ccabb032d1136add
Fatih Kabay <mfthkabay@gmail.com>
2023-04-05 06:10:18 +0300

    remove loading and useState import

--- community-fe/src/layouts/DefaultLayout/index.tsx
+++ community-fe/src/layouts/DefaultLayout/index.tsx
@@ -1,10 +1,9 @@
-import { PropsWithChildren, useState, useEffect } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import {Breadcrumb, Layout, Menu, theme, Button } from 'antd';
 import "./DefaultLayout.css";
 import { clearStorage } from '../../utils/helpers';
 import { useNavigate } from 'react-router-dom';
 import { getUser } from '../../utils/helpers';
-import Loading from "../../components/Loading";
 
 const {Header, Content, Footer, Sider } = Layout;
 
